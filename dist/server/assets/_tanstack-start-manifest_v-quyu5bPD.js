ae07ba92a6b3fd67733da04846b7a62324350c78
jaingarvit945@gmail.com <garvit.jain@openturf.in>
2026-06-23 22:55:40 +0530

    Updated Gitignore

---
+++ dist/server/assets/_tanstack-start-manifest_v-quyu5bPD.js
@@ -0,0 +1,20 @@
+//#region \0tanstack-start-manifest:v
+var tsrStartManifest = () => ({
+	routes: {
+		__root__: {
+			filePath: "/home/garvit-jain/Desktop/Akshin/akshin-our-five-years/src/routes/__root.tsx",
+			children: ["/"],
+			assets: void 0,
+			preloads: ["/assets/index-Kf8uYPYr.js"]
+		},
+		"/": {
+			filePath: "/home/garvit-jain/Desktop/Akshin/akshin-our-five-years/src/routes/index.tsx",
+			children: void 0,
+			assets: void 0,
+			preloads: ["/assets/routes-zXBivHpX.js"]
+		}
+	},
+	clientEntry: "/assets/index-Kf8uYPYr.js"
+});
+//#endregion
+export { tsrStartManifest };
